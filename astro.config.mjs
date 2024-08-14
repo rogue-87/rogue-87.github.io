b34d458628ffadc5aa4cf042eacb822d516dcfbb
Rogue <rogue87.dev@gmail.com>
2024-08-14 17:30:16 +0200

    add sitemap & robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rogue-87.github.io",
   base: "/den/",
+  integrations: [sitemap()],
+  vite: {
+    logLevel: "error",
+  },
 });
