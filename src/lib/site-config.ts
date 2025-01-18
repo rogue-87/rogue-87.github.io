92010f88051cbc3c3380d5c250e6162d7d77a742
Rogue <rogue87.dev@gmail.com>
2025-01-19 01:42:27 +0200

    yet another smol fix

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -3,4 +3,4 @@ import { dev } from "$app/environment";
 export const title = "Rogue's Den";
 export const desc = "just a mere mortal webdev";
 export const author = "Rogue87";
-export const url = dev ? "http://localhost:5173" : "https://rogue87.vercel.app/";
+export const url = dev ? "http://localhost:5173" : "https://rogue87.vercel.app";
