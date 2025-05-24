530a7b62b8d5b6f2fc48db472b7eeaee9fb92f8e
Rogue <rogue87@tuta.io>
2025-05-24 14:07:44 +0200

    rename to posts.json

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoadEvent } from "./$types";
 import type { PostMetadata } from "$lib/types";
 
 export async function load({ fetch }: PageLoadEvent) {
-	const res = await fetch("/api/posts");
+	const res = await fetch("/api/posts.json");
 	const posts: PostMetadata[] = await res.json();
 	return { posts };
 }
